c90a1569e207d12e12bca513de99639323eece89
Isabella-Secchin <bellasecchinvaz@gmail.com>
2024-08-25 19:19:14 -0300

    Letras

---
+++ Letras.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+void Letter_print(char c){
+    if (c == 'A' || c == 'a'){
+        printf("  A A A A A\nA           A\nA           A\nA           A\nA A A A A A A\nA           A\nA           A\nA           A\nA           A\nA           A\n\n\n");
+    }
+    else if (c == 'B' || c == 'b'){
+        printf(" B B B B B\nB          B\nB           B\nB           B\nB           B\nB B B B B B\nB          B\nB           B\nB           B\nB           B\n B B B B B B\n\n\n");
+    }
+    else if (c == 'C' || c == 'c'){
+        printf("   C C C C C\n C           C\nC\nC\nC\nC\nC\nC\n C           C\n   C C C C C\n\n\n");
+    }
+    else if (c == 'D' || c == 'd'){
+        printf("  D D D D D\nD           D\nD            D\nD             D\nD             D\nD             D\nD             D\nD            D\nD           D\n  D D D D D\n\n\n");
+    }
+    else if (c == 'E' || c == 'e'){
+        printf("E E E E E E E\nE\nE\nE\nE\nE E E E E\nE\nE\nE\nE\nE E E E E E E\n\n\n");
+    }
+    else if (c == 'F' || c == 'f'){
+        printf("F F F F F F F\nF\nF\nF\nF\nF F F F F\nF\nF\nF\nF\nF\n\n\n");
+    }
+    else if (c == 'G' || c == 'g'){
+        printf("  G G G G G\nG           G\nG           G\nG\nG\nG\nG        G G G\nG           G\nG           G\n  G G G G G\n\n\n");
+    }
+    else if (c == 'H' || c == 'h'){
+        printf("H           H\nH           H\nH           H\nH           H\nH           H\nH H H H H H H\nH           H\nH           H\nH           H\nH           H\nH           H\n\n\n");
+    }
+    else if (c == 'I' || c == 'i'){
+        printf("I I I I I I I\n      I\n      I\n      I\n      I\n      I\n      I\n      I\n      I\nI I I I I I I\n\n\n");
+    }
+    else if (c == 'J' || c == 'j'){
+        printf("J J J J J J J\n       J\n       J\n       J\n       J\n       J\n       J\n J     J\n  J J J\n\n\n");
+    }
+    else if (c == 'K' || c == 'k'){
+        printf("K          K\nK        K\nK      K\nK    K\nK  K\nK K\nK  K\nK    K\nK      K\nK        K\nK          K\n\n\n");
+    }
+    else if (c == 'L' || c == 'l'){
+        printf("L\nL\nL\nL\nL\nL\nL\nL\nL\nL L L L L L L\n\n\n");
+    }
+    else if (c == 'M' || c == 'm'){
+        printf("M           M\nM M       M M\nM   M   M   M\nM     M     M\nM           M\nM           M\nM           M\nM           M\n\n\n");
+    }
+    else if (c == 'N' || c == 'n'){
+        printf("N           N\nN N         N\nN  N        N\nN   N       N\nN     N     N\nN       N   N\nN        N  N\nN         N N\nN           N\n\n\n");
+    }
+    else if (c == 'O' || c == 'o'){
+        printf("  O O O O O\nO           O\nO           O\nO           O\nO           O\nO           O\nO           O\nO           O\n  O O O O O\n\n\n");
+    }
+    else if (c == 'P' || c == 'p'){
+        printf("P P P P P P\nP           P\nP           P\nP           P\nP P P P P P\nP\nP\nP\nP\nP\n\n\n");
+    }
+    else if (c == 'Q' || c == 'q'){
+        printf("  Q Q Q Q Q\nQ           Q\nQ           Q\nQ           Q\nQ       Q   Q\nQ         Q Q\nQ           Q Q\nQ           Q  Q\n  Q Q Q Q Q\n\n\n");
+    }
+    else if (c == 'R' || c == 'r'){
+        printf("R R R R R R\nR           R\nR           R\nR           R\nR R R R R R\nR   R\nR     R\nR       R\nR         R\nR           R\n\n\n");
+    }
+    else if (c == 'S' || c == 's'){
+        printf("   S S S S S\n S           S\nS\nS\n S\n   S S S S S\n            S\n             S\n             S\n S           S\n   S S S S S\n\n\n");
+    }
+    else if (c == 'T' || c == 't'){
+       printf("T T T T T T T T\n       T\n       T\n       T\n       T\n       T\n       T\n       T\n       T\n\n\n");
+    }
+    else if (c == 'U' || c == 'u'){
+       printf("U           U\nU           U\nU           U\nU           U\nU           U\nU           U\nU           U\n U         U\n   U U U U\n\n\n");
+    }
+    else if (c == 'V' || c == 'v'){
+       printf("V             V\n V           V\n  V         V\n   V       V\n    V     V\n     V   V\n      V V\n       V\n\n\n");
+    }
+    else if (c == 'W' || c == 'w'){
+       printf("W             W\nW             W\nW      W      W\nW     W W     W\nW    W   W    W\nW   W     W   W\nW  W       W  W\n W           W\n\n\n");
+    }
+    else if (c == 'X' || c == 'x'){
+       printf("X             X\n X           X\n  X         X\n   X       X\n    X     X\n     X   X\n       X\n     X   X\n    X     X\n   X       X\n  X         X\n X           X\nX             X\n\n\n");
+    }
+    else if (c == 'Y' || c == 'y'){
+       printf("Y           Y\n  Y       Y\n    Y   Y\n      Y\n      Y\n      Y\n      Y\n      Y\n      Y\n      Y\n\n\n");
+    }
+    else if (c == 'Z' || c == 'z'){
+       printf(" Z Z Z Z Z Z\n         Z\n        Z\n       Z\n      Z\n     Z\n    Z\n   Z\n  Z\n Z Z Z Z Z Z\n\n\n");
+    }
+    else if (c == '-'){
+       printf("\n---------------\n\n\n\n");
+    }
+    else {
+        printf("Letra '%c' nao suportada.\n\n", c);
+    }
+}
+int main(void){
+    char name[200];
+    printf("Digite um nome/frase: ");
+    scanf("%s", name);
+    printf("\n");
+    for (int i = 0; name[i] != '\0'; i++){
+        Letter_print(name[i]);
+    }
+    system("PAUSE");
+    return 0;
+}
