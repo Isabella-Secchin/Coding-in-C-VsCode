bde20223389b6d49964f75505be9a6bc36fe4def
Isabella-Secchin <bellasecchinvaz@gmail.com>
2024-08-29 15:31:52 -0300

    Esbel lista 1 questao 1.

---
+++ Esbel questao 1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+unsigned char geraGreyPixel(int);
+
+int main(void){
+    int tipo;
+    unsigned char pixel;
+    srand(time(NULL));
+    printf("Entre com o tipo para gerar o pixel:\n");
+    scanf("%c", &tipo);
+    pixel = geraGreyPixel(tipo);
+    printf("%u", pixel);
+    return 0;
+}
+
+unsigned char geraGreyPixel(int tipo){
+    int i = rand() % 100 + 1;
+    if(tipo < 0){
+        if(i <= 75){
+            return rand() % 128;
+        }
+        else{
+            return 128 + rand() % (255 - 128 + 1);
+        }
+    }
+    if(tipo > 0){
+        if(i<=75){
+            return 128 + rand() % (255 - 128 + 1);
+        }
+        else{
+            return rand() % 128;
+        }
+    }
+    if(tipo == 0){
+        return rand() % 255 + 1;
+    }
+}
