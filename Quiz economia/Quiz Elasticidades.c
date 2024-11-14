599c008d19becd3e1574b0f77350641053e86ba3
Isabella-Secchin <bellasecchinvaz@gmail.com>
2024-11-13 22:26:23 -0300

    Economia tabela

--- Quiz economia/Quiz Elasticidades.c
+++ Quiz economia/Quiz Elasticidades.c
@@ -28,6 +28,12 @@ void reset(){
 
 int main(void){
   int i, j = 0, k;
+  char array[10];
+  int array2[10];
+  for(i = 0; i <= 10; i++){
+    array[i] = ' ';
+    array2[i] = 0;
+  }
   laranja();
   printf("\n\nQUIZ DE ECONOMIA - Elasticidades\n");
   printf("Grupo: Isabella Secchin, Guilherme Torres, João Rupp e Gustavo Souza\n\n");
@@ -67,17 +73,20 @@ int main(void){
             printf("\nParabéns você acertou a questão! :)\n\n");
             reset();
             j++;
+            array[0] = 'x';
+            array2[0] = (char) k;
             break;
           }
           else{
             laranja();
             printf("\nQue pena, você errou a questão ;(\n\n");
+            array2[0] = (char) k;
             reset();
             break;
           }
         }
         break;
-      case 2:
+      case 1:
         for( ; ; ){
           azul();
           printf("QUESTÃO 2:\n");
@@ -110,17 +119,63 @@ int main(void){
             printf("\nParabéns você acertou a questão! :)\n\n");
             reset();
             j++;
+            array[1] = 'x';
+            array2[1] = (char) k;
             break;
           }
           else{
             laranja();
             printf("\nQue pena, você errou a questão ;(\n\n");
+            array2[1] = (char) k;
             reset();
             break;
           }
         }
         break;
+      case 2:
+
     }
   }
-    return 0;
+  laranja();
+  printf("RESULTADOS:\n\n");
+  reset();
+  branco();
+  printf("         |               |                  |         \n");
+  printf(" QUESTÂO | RESPOSTA DADA | RESPOSTA CORRETA | ACERTOS \n");
+  printf("_________|_______________|__________________|_________\n");
+  printf("         |               |                  |         \n");
+  printf("    1    |       %d       |        4         |    %c   \n", array2[0], array[0]);
+  printf("_________|_______________|__________________|_________\n");
+  printf("         |               |                  |         \n");
+  printf("    2    |       %d       |        2         |    %c   \n", array2[1], array[1]);
+  printf("_________|_______________|__________________|_________\n");
+  printf("         |               |                  |         \n");
+  printf("    3    |       %d       |                  |    %c   \n", array2[2], array[2]);
+  printf("_________|_______________|__________________|_________\n");
+  printf("         |               |                  |         \n");
+  printf("    4    |       %d       |                  |    %c   \n", array2[3], array[3]);
+  printf("_________|_______________|__________________|_________\n");
+  printf("         |               |                  |         \n");
+  printf("    5    |       %d       |                  |    %c   \n", array2[4], array[4]);
+  printf("_________|_______________|__________________|_________\n");
+  printf("         |               |                  |         \n");
+  printf("    6    |       %d       |                  |    %c   \n", array2[5], array[5]);
+  printf("_________|_______________|__________________|_________\n");
+  printf("         |               |                  |         \n");
+  printf("    7    |       %d       |                  |    %c   \n", array2[6], array[6]);
+  printf("_________|_______________|__________________|_________\n");
+  printf("         |               |                  |         \n");
+  printf("    8    |       %d       |                  |    %c   \n", array2[7], array[7]);
+  printf("_________|_______________|__________________|_________\n");
+  printf("         |               |                  |         \n");
+  printf("    9    |       %d       |                  |    %c   \n", array2[8], array[8]);
+  printf("_________|_______________|__________________|_________\n");
+  printf("         |               |                  |         \n");
+  printf("   10    |       %d       |                  |    %c   \n", array2[9], array[9]);
+  printf("         |               |                  |         \n\n");
+  reset();
+  ciano();
+  printf("Quantidade de acertos: %d/10\n\n", j);
+  reset();
+  return 0;
 }
