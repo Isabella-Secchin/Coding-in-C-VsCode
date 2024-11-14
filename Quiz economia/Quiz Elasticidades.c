b9ba83b90398413c5cc6787a5f7f0bc9cbf9e4e0
Isabella-Secchin <bellasecchinvaz@gmail.com>
2024-11-13 21:09:56 -0300

    Quiz economia questao 1

--- Quiz economia/Quiz Elasticidades.c
+++ Quiz economia/Quiz Elasticidades.c
@@ -35,39 +35,48 @@ int main(void){
     for(i = 0; i < 11; i++){
         switch(i){
             case 0:
-            for( ; ; ){
-                azul();
-                printf("QUESTÃO 1:\n");
-                printf("A variação percentual na quantidade demandada, dada a variação percentual no preço de outro bem, se refere a...\n\n");
-                reset();
-                branco();
-                printf("1.Elasticidade-renda da demanda\n");
-                printf("2.Elasticidade-preço da demanda\n");
-                printf("3.Elasticidade-preço da oferta\n");
-                printf("4.Elasticidade-preço cruzada da demanda\n\n");
-                reset();
-                lilas();
-                printf("Entre com a opção correta:");
-                reset();
-                scanf("%d", &k);
-                if(k == 4){
-                    ciano();
-                    printf("\nParabéns você acertou a questão! :)\n\n");
+                for( ; ; ){
+                    azul();
+                    printf("QUESTÃO 1:\n");
+                    printf("A variação percentual na quantidade demandada, dada a variação percentual no preço de outro bem, se refere a...\n\n");
                     reset();
-                    j++;
-                    break;
-                }
-                else if(k > 4 || k < 0){
-                    printf("\nResposta inválida! Insira um número de 1 a 4\n\n");
-                    continue;
-                }
-                else{
-                    laranja();
-                    printf("\nQue pena, você errou a questão ;(\n\n");
+                    branco();
+                    printf("1.Elasticidade-renda da demanda\n");
+                    printf("2.Elasticidade-preço da demanda\n");
+                    printf("3.Elasticidade-preço da oferta\n");
+                    printf("4.Elasticidade-preço cruzada da demanda\n\n");
                     reset();
-                    break;
+                    lilas();
+                    printf("Entre com a opção correta:");
+                    reset();
+                    if(scanf("%d", &k) != 1){
+                        laranja();
+                        printf("\nResposta inválida! Insira um número de 1 a 4\n\n");
+                        reset();
+                        while (getchar() != '\n');
+                        continue;
+                    }
+                    else if( k != 4 && k != 3 && k != 2 && k != 1){
+                        laranja();
+                        printf("\nResposta inválida! Insira um número de 1 a 4\n\n");
+                        reset();
+                        continue;
+                    }
+                    else if(k == 4){
+                        ciano();
+                        printf("\nParabéns você acertou a questão! :)\n\n");
+                        reset();
+                        j++;
+                        break;
+                    }
+                    else{
+                        laranja();
+                        printf("\nQue pena, você errou a questão ;(\n\n");
+                        reset();
+                        break;
+                    }
                 }
-            }
+                break;
             case 2:
         }
     }
